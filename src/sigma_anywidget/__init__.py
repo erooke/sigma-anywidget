e0a7fa4cde8bc59458d4e355edc77399a69f98a2
Ethan Rooke <ethan@roo.ke>
2026-05-30 09:16:04 -0500

    python: init widget

---
+++ src/sigma_anywidget/__init__.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import anywidget
+import traitlets
+
+_STATIC_ROOT: Path = Path(__file__).parent / "static"
+
+
+class SigmaWidget(anywidget.AnyWidget):
+    _esm = _STATIC_ROOT / "widget.js"
+    _css = _STATIC_ROOT / "widget.css"
+    nodes = traitlets.Dict(dict()).tag(sync=True)
+    edges = traitlets.List(list()).tag(sync=True)
+    selected = traitlets.List(list()).tag(sync=True)
+
+    def clear_selection(self):
+        self.send({"type": "clear_selection"})
